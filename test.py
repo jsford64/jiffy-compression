48ab2727e03b7c3df897adb84c4661679569edb6
jsford64 <jsford64@users.noreply.github.com>
2023-05-09 20:42:48 -0400

    Update test.py to include license banner

--- test.py
+++ test.py
@@ -3,6 +3,22 @@ import jiffy as jf
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+    Copyright 2023 Jeff S. Ford and Jordan S. Ford
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
 def fake_scan(shape, precision=1, out_of_range_percent=0.2, dtype=np.float32):
     scan = np.zeros(shape, dtype=np.float32)
     h,w = shape
